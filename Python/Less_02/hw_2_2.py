17f7c8fff9f1aeb259bf2347d22faddeaad144ea
Leonid Novikov <leopold-10@yandex.ru>
2022-12-20 02:02:58 +0300

    Урок 2

---
+++ Python/Less_02/hw_2_2.py
@@ -0,0 +1,17 @@
+# 2. Для списка реализовать обмен значений соседних элементов.
+# Т.е. значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т.д.
+# При нечетном количестве элементов последний сохранить на своем месте.
+# Для заполнения списка элементов необходимо использовать функцию input().
+
+element_count = int(input('Введите количество элементов списка: '))
+my_list = [0]
+i = 0
+element = 0
+while i < element_count:
+    i += 1
+    my_list.append(i)
+
+for el in range(int((len(my_list) - 1) / 2)):
+        my_list[element], my_list[element + 1] = my_list [element + 1], my_list[element]
+        element += 2
+print(my_list)
